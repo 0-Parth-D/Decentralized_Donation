79c1592b440e4a9ecc14df04781924a5f290da01
0-Parth-D <89337450+0-Parth-D@users.noreply.github.com>
2024-01-09 12:22:43 +0530

    Donator id hide

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -87,7 +87,7 @@ const CampaignDetails = () => {
               <div className="mt-[20px] flex flex-col gap-4">
                 {donators.length > 0 ? donators.map((item, index) => (
                   <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4 border-b-[0.25rem]">
-                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator.slice(0, 4)+'xxxxxx'+item.donator.slice(-4)}</p>
                     <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation} ETH</p>
                   </div>
                 )) : (
